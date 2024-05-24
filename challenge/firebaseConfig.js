68e433b038fda4fa951bb3573e317fa8e079a4bd
priyankaingur <priyankaingur@gmail.com>
2024-05-23 17:19:09 -0700

    Configuring the config props to env

--- challenge/firebaseConfig.js
+++ challenge/firebaseConfig.js
@@ -1,12 +1,13 @@
+// firebaseConfig.ts
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCRSaePjmLq_j0jef45Et2fWB7JIFXYdUM",
-    authDomain: "reel-talks.firebaseapp.com",
-    projectId: "reel-talks",
-    storageBucket: "reel-talks.appspot.com",
-    messagingSenderId: "1034777043777",
-    appId: "1:1034777043777:web:d6bb01583f881f85fbf7c0",
-    measurementId: "G-CLFLM72XFF"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 export default firebaseConfig;
